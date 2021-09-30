282f46e44c0931d7878579620e5709bd20e2d7cf
remya <remya@apna.co>
2021-09-30 19:47:30 +0530

    Bottom NavigationBar

--- app/src/main/java/com/remya/communityfordevelopers/LoginActivity.kt
+++ app/src/main/java/com/remya/communityfordevelopers/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.remya.communityfordevelopers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
+        initView()
+    }
+
+    private fun initView() {
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this,DashboardActivity::class.java))
+        }
     }
 }
